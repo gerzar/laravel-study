13306c99a79f9923b2ed3d5a7fe03897f83426fc
gerzar <tralltrangna@gmail.com>
2022-02-28 14:52:43 +0700

    first lesson

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+    <h2>About</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam assumenda cupiditate delectus dolore dolorum id, impedit magni nulla, obcaecati optio perferendis praesentium quidem quos reprehenderit repudiandae saepe sunt voluptates!
+@endsection
